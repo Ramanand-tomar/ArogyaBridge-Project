15208df5e983df9861feddb5818f74068bf3c598
Ramanand Tomar <ramanandtomar1234@gmail.com>
2025-07-08 15:54:21 +0530

    integrating tailwind css into project

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,17 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import React from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import { Web3Provider } from './context/Web3Context.jsx';
+// import {BrowserRouter} from 'react-router-dom';
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <Web3Provider>
+     
+      <App />
+     
+    </Web3Provider>
+  </StrictMode>
+);
